331464d80d789e14f189bba2d0b0dbb26f767b9a
ilhan <ilhan.balci.10@hotmail.com>
2024-08-24 10:05:50 +0300

    update

--- frontend/README.md
+++
@@ -1,5 +0,0 @@
-npm create vite@latest
-npm i react-router-dom react-redux redux @reduxjs/toolkit @preact/signals-react @tanstack/react-query
-npm i --save-dev @preact/signals-react-transform
-
-npm i node nodemon express cors bcrypt cors dotenv express jsonwebtoken mongodb mongoose node cookie-parser
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { signal } from '@preact/signals-react'
+import { configureStore } from '@reduxjs/toolkit'
+import { Provider } from 'react-redux'
+import slice from './slice.js'
+
 import Header from './Components/Header.jsx'
 import Home from './Pages/Home.jsx'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import slice from './slice.js'
-import { Provider } from 'react-redux'
 import './style/style.css'
-import { configureStore } from '@reduxjs/toolkit'
-//import { signal } from '@preact/signals-react'
 
-/*const autoLogin = async () => {
+const autoLogin = async () => {
     let _autoLogin = await fetch(import.meta.env.VITE_REACT_APP_BACKEND_URL + '/auto-login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
@@ -19,7 +20,8 @@ import { configureStore } from '@reduxjs/toolkit'
 
     return _autoLogin.success ? _autoLogin.user : false
 }
-const user = signal(await autoLogin())*/
+
+const user = signal(await autoLogin())
 
 const queryClient = new QueryClient({
     defaultOptions: {
